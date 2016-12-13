59ee605d6603e5c483bfa366f2b3ddeb4619c3a8
MrErHu <471023902@qq.com>
2016-12-13 23:37:37 +0800

    添加两个Model: 包括： Topic.js 帖子 Comment.js 评论

---
+++ model/Comment.js
@@ -0,0 +1,28 @@
+/**
+ * Created by wanglei on 2016/12/13.
+ * 数据结构字段说明 评论
+ * topic ObjectId 对应帖子Id
+ * author ObjectId 对应评论的作者
+ * group Number 对应所属群组：1->论坛  2->问答   3->作品
+ * content String 回复的内容
+ * status Number 贴子的状态 -1表示被删除 0表示正常
+ * createdAt Date 创建时间
+ * favNum 点赞数量
+ */
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var CommentSchema = new Schema({
+    topicId: {type: Schema.ObjectId, ref:'Topic',index: true},
+    author: { type: Schema.ObjectId, ref: 'User'},
+    group: {type: Number, default: 0, index: true},
+    content: String,
+    status: {type: Number, default: 0},
+    createdAt: { type: Date, default: Date.now },
+    favNum: {type:Number, default: 0}
+});
+
+var Comment = mongoose.model('Comment', CommentSchema);
+
+module.exports = Comment;
---
+++ model/Topic.js
@@ -0,0 +1,28 @@
+/**
+ * Created by wanglei on 2016/12/13.
+ * 数据结构字段说明 帖子
+ * title String 标题
+ * author ObjectId 帖子作者
+ * createdAt Date 创建时间
+ * updateAt Date 更新时间(指操作帖子的时间)
+ * watchdeNum Number 帖子创建时间
+ * favUser Array 收藏的用户
+ * content 内容
+ * comment Array 评论
+ */
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var TopicSchema = new Schema({
+    title: {type: String},
+    author: {type: Schema.ObjectId, ref:'User'},
+    createdAt: {type: Date, default: Date.now},
+    updateAt: {type: Date, default: Date},
+    watchedNum: {type: Number, default: 0},
+    favUser: [{type: Schema.ObjectId, ref:'User'}],
+    comment: [{type: Schema.ObjectId, ref:'Comment'}],
+    content: {type: String},
+});
+
+var Topic = mongoose.model('Topic', TopicSchema);
+module.exports = Topic;
