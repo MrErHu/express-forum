f04c30f264a7c68f365e6021cb3840bc2f9fec6e
MrErHu <wanglei_cs@163.com>
2016-12-21 17:19:24 +0800

    增加了论坛帖子的分类

--- routes/index.js
+++ routes/index.js
@@ -10,14 +10,20 @@ var tool = require('../controller/tool');
 var renderLoginUser = tool.renderLoginUser;
 
 router.get('/', function(req, res, next) {
+  var tag = req.query.tag;
+  if(!tag){
+    //若不存在tag,则默认为1
+    tag = 1;
+  }
   //返回初始数据
   var data = {
     title: 'JavaScript',
-    userinfo: req.session.userinfo
+    userinfo: req.session.userinfo,
+    tag: tag
   };
   //渲染当前登录用户
   renderLoginUser(req, data, function (data) {
-    topicController.getTopicList(0,20,1,function (topicList) {
+    topicController.getTopicList(0,20,parseInt(tag),function (topicList) {
       data.topicList = topicList;
       userController.getUserCreditTop(function (userCreditList) {
         data.userCreditList = userCreditList;
